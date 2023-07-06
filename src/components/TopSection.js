3c72b94f244da5f6ffe1a561067abf42c28fd1b7
Kiran-Alex <kiranalexc@gmail.com>
2023-07-06 16:39:00 +0530

    v1-top section alignbmenst and fixes

--- src/components/TopSection.js
+++ src/components/TopSection.js
@@ -12,17 +12,17 @@ export default function TopSection() {
           {/* <source src={video} type="video/mp4" /> */}
         {/* </video> */}
         <div className="leftsection"style = {{marginTop:"5%"}}>
-        <div className="header3" style = {{fontFamily: 'Benzin-Medium'}}>BETA MAINNET IS LIVE </div>
-          <div className="header1" style = {{fontFamily: 'Benzin-Bold',width:'95%'}}>Introducing a New Era of Finance </div>
+        <div className="header3" style = {{fontFamily: 'Benzin-Medium'}}>PUBLIC MAINNET IS LIVE </div>
+          <div className="header1" style = {{fontFamily: 'Benzin-Bold'}}>INTRODUCING A NEW ERA OF FINANCE</div>
           <br />
-          <div className="subhead2" style = {{fontFamily: 'Medium', color:"#B3C5BB"}}>
-          The decentralised bank providing all traditional banking services powered by in-house DeFi protocols.{" "}
+          <div className="subhead2" style = {{ color:"#B3C5BB"}}>
+          Meet the world’s first financial app powered by DeFi protocols that provides banking services on-chain.
           </div>
           <br />
           {/* <br />
           <br /> */}
           <br />
-          <div className="button89"style = {{padding: 0, marginRight: '0.8rem',color:'#5AF698'}}>
+          <div className="button89"style = {{paddingBottom: 25,paddingLeft : 0, marginRight: '2.7rem',color:'#5AF698'}}>
             <a
               href="https://bit.ly/xadefinance"
               style={{ textDecoration: "none", color:'#5AF698'}}
@@ -31,20 +31,21 @@ export default function TopSection() {
               Get Started
             </a>
           </div>
-          <div className="button89">
+          <div className="button89" id="ras" style = {{paddingLeft : 0,paddingBottom:25}} >
             <a
               href="https://medium.com/@XadeFinance/what-is-xade-and-why-do-you-need-it-b483f46ba5a1"
-              style={{ textDecoration: "none", color:'#5AF698' }}
+              style={{ textDecoration: "none", color:'#E3E3E3' ,      opacity: 0.699999988079071}}
             >
-                <img src = {process.env.PUBLIC_URL + '/Media/i2.png'} style = {{marginRight: '0.7rem', height:'1.2rem', marginBottom: '4px'}}/>
-              What is Xade?
+                <img src = {process.env.PUBLIC_URL + '/Media/File_dock_fill.png'} style = {{marginRight: '0.7rem', height:'1.2rem', marginBottom: '4px'}}/>
+              Read About Us
             </a>
           </div>
         </div>
       <div style = {{color: 'white'}} className="rightsection">
-        <img src = {process.env.PUBLIC_URL + '/Media/firstpage.png'} alt="xade"/>
+        <img src = {process.env.PUBLIC_URL + '/Media/mainimg.png'} alt="xade"/>
       </div>
       </div>
+      <br/>
     </>
   );
 }
