1d473b091b4af8282f29116951acb3339fc6ca5d
Kiran-Alex <kiranalexc@gmail.com>
2023-07-12 19:57:32 +0530

    side panel and top bar completely done

--- src/components/Derivex.js
+++ src/components/Derivex.js
@@ -24,13 +24,77 @@ const Derivex = () => {
     const onChange = (newValue) => {
         setInputValue(newValue);
     };
+
     return (
         <>
             <div className='lol' id='derivexlol' style={{ marginTop: "7em" }}>
                 {/* twap - trading view widget and panel */}
                 <div className='tvwap'>
                     <div className='tvw'>
-                        <TradingViewWidget />
+                        <div className="tvwc">
+                            <div className='tvwch'>
+                                <div className='tvwch-1'>
+                                    <div className='tvwch-1c'>
+                                        <img src='https://gains.trade//_next/static/media/btc.fdaa3ece.svg'/>
+                                        <span>BTC/USD</span>
+                                    </div>
+                                </div>
+                                <div className='tvwch-2'>
+                                        <div className='tvwch-2-1'>
+                                            <span>30838.4</span>
+                                        </div>
+                                        <div className='tvwch-2-2'>
+                                           <span>+429.2</span>
+                                        </div>
+                                </div>
+                                <div className='tvwch-2'>
+                                        <div className='tvwch-3-1'>
+                                            <span>Open Interest (L)</span>
+                                        </div>
+                                        <div className='tvwch-3-2'>
+                                           <span>14.6M / 22M</span>
+                                        </div>
+                                </div>
+
+                                <div className='tvwch-2'>
+                                        <div className='tvwch-3-1'>
+                                            <span>Open Interest (S)</span>
+                                        </div>
+                                        <div className='tvwch-3-2'>
+                                           <span>361.6k / 22M</span>
+                                        </div>
+                                </div>
+
+                                <div className='tvwch-2'>
+                                        <div className='tvwch-3-1'>
+                                            <span>Borrowing (L)</span>
+                                        </div>
+                                        <div className='tvwch-4-2'>
+                                           <span>0.0067%</span>
+                                        </div>
+                                </div>
+
+                                <div className='tvwch-2'>
+                                        <div className='tvwch-3-1'>
+                                            <span>Borrowing (S)</span>
+                                        </div>
+                                        <div className='tvwch-4-2'>
+                                           <span>0.0000%</span>
+                                        </div>
+                                </div>
+
+                                <div className='tvwch-2'>
+                                        <div className='tvwch-3-1'>
+                                            <span>Rollover</span>
+                                        </div>
+                                        <div className='tvwch-5-2'>
+                                           <span>0.0033%</span>
+                                        </div>
+                                </div>
+                                
+                            </div>
+                            <TradingViewWidget />
+                        </div>
                         <div className="tvwns">
                             <div className="tvwnsh">
                                 <div className='tvwnshl'>
@@ -134,7 +198,7 @@ const Derivex = () => {
                                                 backgroundColor: "black",
                                                 outline: "none",
                                                 borderStyle: "none",
-                                                color : "white"
+                                                color: "white"
                                             }} onChange={onIChange} />
                                         </div>
                                     </div>
@@ -146,22 +210,22 @@ const Derivex = () => {
                                             Stop Loss <span className='tvwpht2-s1-span'>(None)</span>
                                         </div>
                                         <div className='tvwpht2-s1-2'>
-                                        <span className='tvwpht2-s1-span'>None</span>
+                                            <span className='tvwpht2-s1-span'>None</span>
                                         </div>
                                     </div>
                                     <div className='tvwpht2-s2'>
-                                            
-                                                <div className='tvwpht2-s2-1'>
-                                                    
-                                                <button className='tvwpht2-s2-1-c' >NONE</button>
-                                                <button className='tvwpht2-s2-1-c' >-10%</button>
-                                                <button className='tvwpht2-s2-1-c' >-25%</button>
-                                                <button className='tvwpht2-s2-1-c' >-50%</button>
-                                                <button className='tvwpht2-s2-1-c' >-75%</button>
-                                                <input className='tvwpht2-s2-1-c' type='text' placeholder='PRICE'/>
-                                            </div>
+
+                                        <div className='tvwpht2-s2-1'>
+
+                                            <button className='tvwpht2-s2-1-c' >NONE</button>
+                                            <button className='tvwpht2-s2-1-c' >-10%</button>
+                                            <button className='tvwpht2-s2-1-c' >-25%</button>
+                                            <button className='tvwpht2-s2-1-c' >-50%</button>
+                                            <button className='tvwpht2-s2-1-c' >-75%</button>
+                                            <input className='tvwpht2-s2-1-c' type='text' placeholder='PRICE' />
+                                        </div>
                                     </div>
-                                    
+
                                 </div>
                                 <div className='tvwpht2-t'>
 
@@ -170,22 +234,82 @@ const Derivex = () => {
                                             Take Profit <span className='tvwpht2-t1-span'>(168166)</span>
                                         </div>
                                         <div className='tvwpht2-t1-2'>
-                                        <span className='tvwpht2-t1-span'>None</span>
+                                            <span className='tvwpht2-t1-span'>None</span>
                                         </div>
                                     </div>
                                     <div className='tvwpht2-t2'>
-                                            
-                                                <div className='tvwpht2-t2-1'>
-                                                    
-                                                <button className='tvwpht2-t2-1-c' >25%</button>
-                                                <button className='tvwpht2-t2-1-c' >50%%</button>
-                                                <button className='tvwpht2-t2-1-c' >100%</button>
-                                                <button className='tvwpht2-t2-1-c' >300%</button>
-                                                <button className='tvwpht2-t2-1-c' >900%</button>
-                                                <input className='tvwpht2-t2-1-c' type='text' placeholder='PRICE'/>
-                                            </div>
+
+                                        <div className='tvwpht2-t2-1'>
+
+                                            <button className='tvwpht2-t2-1-c' >25%</button>
+                                            <button className='tvwpht2-t2-1-c' >50%%</button>
+                                            <button className='tvwpht2-t2-1-c' >100%</button>
+                                            <button className='tvwpht2-t2-1-c' >300%</button>
+                                            <button className='tvwpht2-t2-1-c' >900%</button>
+                                            <input className='tvwpht2-t2-1-c' type='text' placeholder='PRICE' />
+                                        </div>
                                     </div>
-                                    
+
+                                </div>
+
+                                <div className="tvwphyt2-mc">
+                                    <div className='tvwphyt2-m'>
+                                        <span className='tvwphyt2-m-spanl' id='tvwphyt2-m-spanl-hl'>
+                                            BTC/USD
+                                        </span>
+                                        <span className='tvwphyt2-m-spanr' id='tvwphyt2-m-spanl-hr'>
+                                            WALLET NOT CONNECTED
+                                        </span>
+                                    </div>
+                                    <div className='tvwphyt2-m'>
+                                        <span className='tvwphyt2-m-spanl'>
+                                            EST. EXECUTION PRICE
+                                        </span>
+                                        <span className='tvwphyt2-m-spanr'>
+                                            30740.6
+                                        </span>
+                                    </div>
+                                    <div className='tvwphyt2-m'>
+                                        <span className='tvwphyt2-m-spanl'>
+                                            SPREAD
+                                        </span>
+                                        <span className='tvwphyt2-m-spanr'>
+                                            0.04%
+                                        </span>
+                                    </div>
+                                    <div className='tvwphyt2-m'>
+                                        <span className='tvwphyt2-m-spanl'>
+                                            POSITION SIZE
+
+                                        </span>
+                                        <span className='tvwphyt2-m-spanr'>
+                                            100 DAI
+                                        </span>
+                                    </div>
+                                    <div className='tvwphyt2-m'>
+                                        <span className='tvwphyt2-m-spanl'>
+                                            FEES
+                                        </span>
+                                        <span className='tvwphyt2-m-spanr'>
+                                            0.1 DAI
+                                        </span>
+                                    </div>
+                                    <div className='tvwphyt2-m'>
+                                        <span className='tvwphyt2-m-spanl'>
+                                            LIQ. PRICE
+                                        </span>
+                                        <span className='tvwphyt2-m-spanr'>
+                                            16907.6
+                                        </span>
+                                    </div>
+                                    <div className='tvwphyt2-m'>
+                                        <span className='tvwphyt2-m-spanl'>
+                                            EST. BORROWING FEE / H
+                                        </span>
+                                        <span className='tvwphyt2-m-spanr'>
+                                            0.0 DAI
+                                        </span>
+                                    </div>
                                 </div>
                             </div>
                         </div>
